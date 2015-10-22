288db23b51737c0d323dbed3b27dae63dc9c987b
Simon Stubben <simonstubben@gmail.com>
2015-10-22 16:10:14 +0800

    Finish login/logout

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,6 +6,8 @@ class SessionController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       login user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
@@ -14,7 +16,7 @@ class SessionController < ApplicationController
   end
 
   def destroy
-    logout
+    logout if logged_in?
     redirect_to root_url
   end
 end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,11 +1,32 @@
 module SessionHelper
-
+  # Logs in the given user.
   def login(user)
     session[:user_id] = user.id
   end
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        login user
+        @current_user = user
+      end
+    end
+
   end
 
   def logged_in?
@@ -13,6 +34,7 @@ module SessionHelper
   end
 
   def logout
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
