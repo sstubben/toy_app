54a8aaf0d07ac00be8d35f55c493c920bf917ad9
Simon Stubben <simonstubben@gmail.com>
2015-10-23 17:46:14 +0800

    Fixed tests

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,12 +3,12 @@ class SessionController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      login user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      login @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      remember @user
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
